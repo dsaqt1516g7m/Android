916e1d4ed3fc20ec131548105a3dd31161271df1
Carolina Cerro <carolbcn88@gmail.com>
2016-01-18 19:39:39 +0100

    Comments Error al Listar

---
+++ Where/app/src/main/java/edu/upc/eetac/dsa/where/Client/Entity/CommentsCollection.java
@@ -0,0 +1,7 @@
+package edu.upc.eetac.dsa.where.Client.Entity;
+
+/**
+ * Created by Carolina on 18/01/2016.
+ */
+public class CommentsCollection {
+}
---
+++ Where/app/src/main/java/edu/upc/eetac/dsa/where/CommentsCollectionAdapter.java
@@ -0,0 +1,7 @@
+package edu.upc.eetac.dsa.where;
+
+/**
+ * Created by Carolina on 18/01/2016.
+ */
+public class CommentsCollectionAdapter {
+}
